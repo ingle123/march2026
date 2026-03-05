2be896ccc7bb1eac1330775d3ae4d8afe071e9ca
sachin <ingle.sachin0@gmail.com>
2026-03-05 12:18:41 +0530

    updated

--- prselenium2026/src/test/java/presl/click.java
+++ prselenium2026/src/test/java/presl/click.java
@@ -9,6 +9,7 @@ public class click {
 	public static void main(String[] args) throws InterruptedException {
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://www.facebook.com/");
+	driver.get("https://www.facebook.com/");
 	driver.manage().window().maximize();
 	driver.findElement(By.xpath("//span[contains(text(),'Log in')]")).click();
 	Thread.sleep(5000);
